6208da6be162a45ec5c76769a1a4471609e6f336
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-12 12:57:34 -0700

    completes get and post endpoints

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -2,11 +2,37 @@ const express = require("express");
 const db = require("../data/dbConfig.js");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+// adding first() in get /
+
+router.get("/", (req, res) => {
+  db.select("id", "name", "budget")
+    .from("accounts")
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error retrieving the accounts." });
+    });
+});
 
 router.get("/:id", (req, res) => {});
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const account = req.body;
+
+  db("accounts")
+    .insert(account)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error adding the account." });
+    });
+});
 
 router.put("/:id", (req, res) => {});
 
